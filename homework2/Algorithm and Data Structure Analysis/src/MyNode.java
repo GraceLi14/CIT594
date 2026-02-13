d56fee710e07350391046c3acc0249f396639694
GraceLi14 <graceli166@gmail.com>
2026-02-13 17:22:21 -0500

    Homework 2 (MyNode + MyTree contain method)

--- homework2/Algorithm and Data Structure Analysis/src/MyNode.java
+++ homework2/Algorithm and Data Structure Analysis/src/MyNode.java
@@ -1,12 +1,35 @@
+/**
+ * Represents a single node in a binary search tree.
+ *
+ * Each node stores:
+ * - A data item of generic type T
+ * - A reference to its left child
+ * - A reference to its right child
+ * - A reference to its parent node
+ *
+ * @param <T> the type of data stored in the node
+ */
 public class MyNode<T>{
 
     //fields
+
+    //Data stored in this node
     private T item;
+    //reference to left child
     private MyNode<T> left;
+    //reference to right child
     private MyNode<T> right;
+    //reference to parent node (null if node is the root)
     private MyNode<T> parent;
 
     //constructor
+
+    /**
+     * Constructs a new node containing the specified item.
+     * The left, right, and parent references are initialized to null.
+     *
+     * @param item the data to store in this node
+     */
     public MyNode(T item) {
         this.item = item;
         this.left = null;
@@ -15,32 +38,69 @@ public class MyNode<T>{
     }
 
     //getters
+
+    /**
+     * Returns the item stored in this node.
+     *
+     * @return the data contained in this node
+     */
     public T getItem(){
         return this.item;
     }
 
+    /**
+     * Returns the left child of this node.
+     *
+     * @return the left child node, or null if none exists
+     */
     public MyNode<T> getLeft(){
         return this.left;
     }
 
+    /**
+     * Returns the right child of this node.
+     *
+     * @return the right child node, or null if none exists
+     */
     public MyNode<T> getRight(){
         return this.right;
     }
 
+    /**
+     * Returns parent of this node.
+     * @return
+     */
     public MyNode<T> getParent(){
         return this.parent;
     }
 
     //setters
 
+    /**
+     * Sets the left child of this node.
+     * Note: This method does not automatically update the child's parent reference.
+     *
+     * @param input the node to set as the left child
+     */
     public void setLeft(MyNode<T> input){
         this.left = input;
     }
 
+    /**
+     * Sets the right child of this node.
+     * Note: This method does not automatically update the child's parent reference.
+     *
+     * @param input the node to set as the right child
+     */
     public void setRight(MyNode<T> input){
         this.right = input;
     }
 
+    /**
+     * Sets the parent of this node.
+     *
+     * @param input the node to set as the parent
+     */
     public void setParent(MyNode<T> input){
         this.parent = input;
     }
--- homework2/Algorithm and Data Structure Analysis/src/MyTree.java
+++ homework2/Algorithm and Data Structure Analysis/src/MyTree.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+
+/**
+ *
+ * @param <T>
+ */
 public class MyTree <T extends Comparable<T>> {
     //fields
 
@@ -11,20 +17,61 @@ public class MyTree <T extends Comparable<T>> {
     //methods
 
     /**
+     * Inserts the specified item into the binary search tree.
+     * <p>
+     * The tree maintains the BST ordering property:
+     * - All values in the left subtree are less than the parent node.
+     * - All values in the right subtree are greater than the parent node.
+     * <p>
+     * If the item already exists in the tree, no new node is created
+     * and the existing node is returned.
      *
-     * @param item
-     * @return
+     * @param item the value to insert into the tree
+     * @return the newly created node containing the item, or the
+     * existing node if the item is already present
+     * @throws IllegalArgumentException if item is null
      */
-    public MyNode insert(T item){
+
+    public MyNode<T> insert(T item) {
 
         // If item is null, throw IllegalArgumentException
         if (item == null) {
             throw new IllegalArgumentException("Item cannot be null.");
         }
 
-        MyNode<T> node = this.root;
+        MyNode<T> insertNode = new MyNode<>(item);
 
+        if (this.root == null) {
+            this.root = insertNode;
+            return insertNode;
+        }
 
+        MyNode<T> current = this.root;
+
+        while (current != null) {
+
+            if (item.compareTo(current.getItem()) < 0) {
+                if (current.getLeft() == null) {
+                    current.setLeft(insertNode);
+                    insertNode.setParent(current);
+                    return insertNode;
+                }
+                current = current.getLeft();
+
+            } else if (item.compareTo(current.getItem()) > 0) {
+                if (current.getRight() == null) {
+                    current.setRight(insertNode);
+                    insertNode.setParent(current);
+                    return insertNode;
+                }
+                current = current.getRight();
+
+            } else {
+                return current;
+            }
+        }
+        //unreachable in correct logic
+        return null;
     }
 
     /**
@@ -55,16 +102,69 @@ public class MyTree <T extends Comparable<T>> {
             if (item.compareTo(current.getItem()) < 0) {
                 current = current.getLeft();
 
-            //Compare target item with current node's item, if target larger, move to right subtree
+                //Compare target item with current node's item, if target larger, move to right subtree
             } else if (item.compareTo(current.getItem()) > 0) {
                 current = current.getRight();
 
-            //If comparison==0 (implicit in else) item has been found
+                //If comparison==0 (implicit in else) item has been found
             } else {
                 return current;
             }
         }
         //If traversal reaches null, the item is not in the tree so return null
         return null;
+    }
+
+    /**
+     *
+     * @param item
+     * @return
+     */
+    public boolean remove(T item) {
+
+
+    }
+
+    public void inOrderTraversal(MyNode<T> node, StringBuilder sb){
+
+        if (node == null) {
+            return;
+        }
+
+
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String toString() {
+
+        if (this.root == null) {
+            return "";
+        }
+
+        MyNode<T> current = this.root;
+
+        ArrayList<T> stringTracker = new ArrayList<>();
+        if (current.getLeft() != null) {
+            current = current.getLeft();
+
+        }
+
+
+    }
+
+    /**
+     * Returns the root node of the binary search tree.
+     *
+     * If the tree is empty, this method returns null.
+     *
+     * @return the root node of the tree, or null if the tree is empty
+     */
+    public MyNode getRoot() {
+        // Simply return the root reference.
+        // If the tree is empty, root will be null.
+        return this.root;
     }
 }
